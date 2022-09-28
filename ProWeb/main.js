03becbbb23f4c582874efbc0644cd65d5c3d2b05
Arian <arian.koochak@gmail.com>
2022-09-28 19:07:57 +0330

    init

--- ProWeb/main.js
+++ ProWeb/main.js
@@ -7,9 +7,9 @@ function movingObject(e){
     let mouseY = (window.innerHeight - e.clientY) / 100;
     objs[1].style.transform = `translate(${mouseX}px,${-mouseY}px)`
     objs[2].style.transform = `translate(${-mouseX}px,${mouseY}px)`
-    objs[3].style.transform = `translate(${-mouseX}px,${-mouseY}px)`
+    objs[3].style.transform = `translate(${mouseX}px,${mouseY}px)`
     objs[4].style.transform = `translate(${mouseX}px,${-mouseY}px)`
-    objs[5].style.transform = `translate(${mouseX}px,${mouseY}px)`
+    objs[5].style.transform = `translate(${-mouseX}px,${-mouseY}px)`
 }
 
 window.addEventListener("mousemove",movingObject);
