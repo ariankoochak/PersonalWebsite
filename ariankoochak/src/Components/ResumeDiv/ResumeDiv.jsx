80a80d4e36620ab0d98d0cf6175069ae97405437
Arian <45326611+ariankoochak@users.noreply.github.com>
2024-01-05 14:41:11 +0330

    add resume div

---
+++ ariankoochak/src/Components/ResumeDiv/ResumeDiv.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { useSelector } from 'react-redux';
+
+export default function ResumeDiv() {
+    const mainColor = useSelector((store) => store.colorSlice.mainColor);
+  return (
+      <div
+          className={`common-styles-div resume-div ${mainColor.backgroundColorDiv}`}
+      >
+          <h2 className={`${mainColor.color}`}>Resume</h2>
+          <div className="resume-data">
+              <div className="age">
+                  <span className="title">Age</span>
+                  <span className="value">20Years</span>
+              </div>
+              <div className="city">
+                  <span className="title">City</span>
+                  <span className="value">Iran,Tehran</span>
+              </div>
+              <div className="military">
+                  <span className="title">military service status</span>
+                  <span className="value">education pardon</span>
+              </div>
+              <div className="email">
+                  <span className="title">Email</span>
+                  <span className="value">arian.koochak@protonmail.com</span>
+              </div>
+          </div>
+      </div>
+  );
+}
--- ariankoochak/src/Layouts/HomePage/HomePage.jsx
+++ ariankoochak/src/Layouts/HomePage/HomePage.jsx
@@ -3,23 +3,25 @@ import TitleDiv from "../../Components/TitleDiv/TitleDiv";
 import ImageDiv from "../../Components/ImageDiv/ImageDiv";
 import ContactMeDiv from "../../Components/ContactMeDiv/ContactMeDiv";
 import SocialsDiv from "../../Components/SocialsDiv/SocialsDiv";
+import ResumeDiv from "../../Components/ResumeDiv/ResumeDiv";
 
 
 export default function HomePage() {
     return (
         <>
-            <section className="container">
-              <article className="contact-and-me-data">
-                    <TitleDiv/>
-                    <div className="image-and-contact-container">
-                        <ImageDiv/>
-                        <ContactMeDiv/>
-                    </div>
-                    <SocialsDiv/>
-              </article>
-              <article className="about-myskills-certificates-data">
-
-              </article>
+            <section className="main">
+                <section className="container">
+                    <article className="contact-and-me-data">
+                        <TitleDiv />
+                        <div className="image-and-contact-container">
+                            <ImageDiv />
+                            <ContactMeDiv />
+                        </div>
+                        <SocialsDiv />
+                        <ResumeDiv/>
+                    </article>
+                    <article className="about-myskills-certificates-data"></article>
+                </section>
             </section>
         </>
     );
