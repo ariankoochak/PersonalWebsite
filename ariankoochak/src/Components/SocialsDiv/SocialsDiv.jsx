2cc5b7064b40e267c7f59672f7f8b33396313742
Arian <45326611+ariankoochak@users.noreply.github.com>
2024-01-05 20:38:49 +0330

    add socials links

--- ariankoochak/src/Components/SocialsDiv/SocialsDiv.jsx
+++ ariankoochak/src/Components/SocialsDiv/SocialsDiv.jsx
@@ -7,18 +7,28 @@ export default function SocialsDiv() {
       <div
           className={`common-styles-div socials-div ${mainColor.backgroundColorDiv}`}
       >
+          <a href="https://www.instagram.com/ariankoochak/" target='_blank' rel="noreferrer">
           <div className="instagram">
               <i className="fa-brands fa-instagram"></i>
           </div>
+          </a>
+
+          <a href="https://www.linkedin.com/in/arian-koochak-281b36240/" target='_blank' rel="noreferrer">
           <div className="linkedin">
               <i className="fa-brands fa-linkedin-in"></i>
           </div>
-          <div className="github">
-              <i className="fa-brands fa-github"></i>
-          </div>
+          </a>
+
+          <a href="https://github.com/ariankoochak" target='_blank' rel="noreferrer">
+              <div className="github" >
+                  <i className="fa-brands fa-github"></i>
+              </div>
+          </a>
+          <a href="https://quera.org/profile/DULDQ" target='_blank' rel="noreferrer">
           <div className="quera">
-            <img src={queraImageSrc} alt="quera" />
+              <img src={queraImageSrc} alt="quera" />
           </div>
+          </a>
       </div>
   );
 }
