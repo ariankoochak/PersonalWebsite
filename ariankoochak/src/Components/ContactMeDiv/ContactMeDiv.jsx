ce1e92bf7e2d63e21b1e770095a47659e2f7d36a
Arian <45326611+ariankoochak@users.noreply.github.com>
2024-01-05 12:42:15 +0330

    add photo and contact me divs

--- ariankoochak/src/Components/ContactMeDiv/ContactMeDiv.jsx
+++ ariankoochak/src/Components/ContactMeDiv/ContactMeDiv.jsx
@@ -1,7 +1,22 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
 
 export default function ContactMeDiv() {
+    const mainColor = useSelector((store) => store.colorSlice.mainColor);
+
   return (
-    <div>ContactMeDiv</div>
-  )
+      <div
+          className={`common-styles-div contact-div ${mainColor.backgroundColorDiv}`}
+      >
+          <div className="contact-container">
+              <span>I’m available for work</span>
+              <button className="contact-me-btn">Contact me</button>
+              <button
+                  className={`download-cv-btn ${mainColor.backgroundColor}`}
+              >
+                  Download CV
+              </button>
+          </div>
+      </div>
+  );
 }
--- ariankoochak/src/Components/ImageDiv/ImageDiv.jsx
+++ ariankoochak/src/Components/ImageDiv/ImageDiv.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import { useSelector } from "react-redux";
-import profilePhotoSrc from '../../Assets/Images/profile.jpg'
 
 export default function ImageDiv() {
     const mainColor = useSelector((store) => store.colorSlice.mainColor);
 
   return (
       <div className={`common-styles-div image-div ${mainColor.backgroundColorDiv}`}>
-          <img src={profilePhotoSrc} alt="arian koochak"/>
       </div>
   );
 }
