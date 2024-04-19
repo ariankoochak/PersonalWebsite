d7794c717358751dcbe5513127ae814a9935eeb0
Arian <45326611+ariankoochak@users.noreply.github.com>
2024-04-19 16:06:15 +0330

    Update AboutDiv.jsx

--- ariankoochak/src/Components/AboutDiv/AboutDiv.jsx
+++ ariankoochak/src/Components/AboutDiv/AboutDiv.jsx
@@ -13,12 +13,12 @@ export default function AboutDiv() {
               I started programming with C++ when I was 12 years old. During
               high school, I participated in global and domestic RoboCup
               competitions and won a place. In these competitions, I
-              participated in the rescue simulation open league (Co-Space) And
-              I started working with Javascript and I did various projects with
-              Javascript, React js, Node js, Mysql, which you can see in my GitHub.
-              Now I am studying computer engineering in Tehran Azad University
-              of Research Sciences And in the future, I want to enter and work
-              more in the fields of AI and blockchain.
+              participated in the rescue simulation open league (Co-Space) And I
+              started working with Javascript and I did various projects with
+              Javascript, React js, Node js, Mysql, which you can see in my
+              GitHub. Now I am studying computer engineering in Tehran Azad
+              University of Research Sciences And in the future, I want to enter
+              and work more in the field of blockchain
           </p>
       </div>
   );
