99373c7459ba8498d2eecaa0ebc1459fc3240d28
Arian <45326611+ariankoochak@users.noreply.github.com>
2024-06-18 16:03:23 +0330

    add cloud button and delete cloud route

--- ariankoochak/src/App.jsx
+++ ariankoochak/src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import './Assets/Styles/global.css';
 import { BrowserRouter, Route, Routes} from "react-router-dom";
 import HomePage from './Layouts/HomePage/HomePage'
-import CloudPage from './Layouts/CloudPage/CloudPage';
 
 export default function App() {
   return (
@@ -10,7 +9,6 @@ export default function App() {
           <BrowserRouter>
               <Routes>
                   <Route path="/" element={<HomePage />}></Route>
-                  <Route path="/cloud" element={<CloudPage />}></Route>
               </Routes>
           </BrowserRouter>
       </>
--- ariankoochak/src/Components/ColorChanger/ColorChanger.jsx
+++ ariankoochak/src/Components/ColorChanger/ColorChanger.jsx
@@ -1,55 +1,65 @@
-import React, { useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux';
-import { setBlue, setGreen, setOrange, setRed } from '../../utils/store/slices/colorSlice';
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+    setBlue,
+    setGreen,
+    setOrange,
+    setRed,
+} from "../../utils/store/slices/colorSlice";
 
 export default function ColorChanger() {
-    const colors = ['blue','red','green','orange']
+    const colors = ["blue", "red", "green", "orange"];
     const mainColor = useSelector((store) => store.colorSlice.mainColor);
     const [selectedColor, setSelectedColor] = useState(mainColor.name);
     const dispatch = useDispatch();
-    const handleColorClick = (e)=>{
+    const handleColorClick = (e) => {
         switch (e.target.id) {
             case "blue":
-                setSelectedColor('blue')
+                setSelectedColor("blue");
                 dispatch(setBlue());
                 break;
             case "red":
-                setSelectedColor('red')
+                setSelectedColor("red");
                 dispatch(setRed());
                 break;
             case "green":
-                setSelectedColor('green')
+                setSelectedColor("green");
                 dispatch(setGreen());
                 break;
             case "orange":
-                setSelectedColor('orange')
+                setSelectedColor("orange");
                 dispatch(setOrange());
                 break;
             default:
                 console.error("error in handleColorClick function");
         }
-    }
-    const handleRenderColors = ()=>{
-        return colors.map((color)=>{
-          return (
-              <div
-                  id={color}
-                  key={color}
-                  className={`persian-${color}-Backgroundcolor ${color === selectedColor ? 'selected-color' : ''}`}
-                  onClick={handleColorClick}
-              ></div>
-          ); 
-        })
-    }
-  return (
-      <div
-          className={`common-styles-div color-changer-div ${mainColor.backgroundColorDiv}`}
-      >
-        {handleRenderColors()}
-          {/* <div id = 'blue' className="persian-blue-Backgroundcolor selected-color" onClick={handleColorClick}></div>
+    };
+    const handleRenderColors = () => {
+        return colors.map((color) => {
+            return (
+                <div
+                    id={color}
+                    key={color}
+                    className={`persian-${color}-Backgroundcolor ${
+                        color === selectedColor ? "selected-color" : ""
+                    } color`}
+                    onClick={handleColorClick}
+                ></div>
+            );
+        });
+    };
+    return (
+        <div
+            className={`common-styles-div color-changer-div ${mainColor.backgroundColorDiv}`}
+        >
+            <div className="left-side">{handleRenderColors()}</div>
+            <div className="right-side">
+                <a href="https://mega.nz/folder/yQ11XJ6a#HKqEfDjBETz9vbxFVvbBMA" target="_blank" rel="noreferrer"> &#9729; &#65039;</a>
+            </div>
+            {/* <div id = 'blue' className="persian-blue-Backgroundcolor selected-color" onClick={handleColorClick}></div>
           <div id = 'red' className="persian-red-Backgroundcolor" onClick={handleColorClick}></div>
           <div id = 'green' className="persian-green-Backgroundcolor" onClick={handleColorClick}></div>
           <div id = 'orange' className="persian-orange-Backgroundcolor" onClick={handleColorClick}></div> */}
-      </div>
-  );
+        </div>
+    );
 }
