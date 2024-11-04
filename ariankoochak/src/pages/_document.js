3a55066bce41d527a444373fb46ceb4f1cf0bf3a
Arian <arian.koochak@protonmail.com>
2024-11-04 16:09:07 +0330

    add title and favicon to head html

--- ariankoochak/src/pages/_document.js
+++ ariankoochak/src/pages/_document.js
@@ -4,6 +4,8 @@ export default function Document() {
   return (
       <Html lang="en">
           <Head />
+          <title>Arian Koochak</title>
+          <link rel="icon" href="/favicon.ico" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
           <link
