a0b9ef1b0a1def160323ba557dc69bba89cadd2f
Arian <arian.koochak@protonmail.com>
2024-11-04 15:21:30 +0330

    config fontawesome

--- ariankoochak/src/pages/_app.js
+++ ariankoochak/src/pages/_app.js
@@ -1,4 +1,10 @@
 import "@/styles/globals.css";
+import "@/styles/fontawesome/css/fontawesome.css";
+import "@/styles/fontawesome/css/brands.css";
+import "@/styles/fontawesome/css/solid.css";
+import "@/styles/fontawesome/css/v5-font-face.css";
+import "@/styles/fontawesome/css/all.css";
+import "@/styles/fontawesome/css/v4-shims.css";
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
