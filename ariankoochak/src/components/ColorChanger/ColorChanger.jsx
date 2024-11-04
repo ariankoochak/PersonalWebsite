722361cc5facc21cdd572dbeb36da83dbdbf0236
Arian <arian.koochak@protonmail.com>
2024-11-04 15:09:24 +0330

    add ColorChanger Components

---
+++ ariankoochak/src/components/ColorChanger/ColorChanger.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+export default function ColorChanger() {
+    const colors = ["blue", "red", "green", "orange"];
+    
+    const handleRenderColors = () => {
+        return colors.map((color) => {
+            return (
+                <div
+                    id={color}
+                    key={color}
+                    className={`persian-${color}-Backgroundcolor color`}
+                ></div>
+            );
+        });
+    };
+    return (
+        <div
+            className={`common-styles-div color-changer-div persian-blue-div-color`}
+        >
+            <div className="left-side">{handleRenderColors()}</div>
+            <div className="right-side">
+                <a
+                    href="https://mega.nz/folder/yQ11XJ6a#HKqEfDjBETz9vbxFVvbBMA"
+                    target="_blank"
+                    rel="noreferrer"
+                >
+                    ☁️
+                </a>
+            </div>
+        </div>
+    );
+}
---
+++ ariankoochak/src/pages/index.jsx
@@ -0,0 +1,29 @@
+import ColorChanger from "@/components/ColorChanger/ColorChanger";
+
+export default function HomePage() {
+  return (
+      <section className="main">
+          <section className="container">
+              <ColorChanger />
+              {/* <article className="contact-and-me-data">
+                  <TitleDiv />
+                  <div className="image-and-contact-container">
+                      <ImageDiv />
+                      <ContactMeDiv />
+                  </div>
+                  <SocialsDiv />
+                  <ResumeDiv />
+              </article>
+              <article className="about-myskills-certificates-data">
+                  <div className="up-side">
+                      <MySkillsDiv />
+                      <AboutDiv />
+                  </div>
+                  <div className="down-side">
+                      <DatasDiv />
+                  </div>
+              </article> */}
+          </section>
+      </section>
+  );
+}
